c2ea6b66c6cfbbadb770a2058711d3b6f24133eb
Rii0304 <148870319+Rii0304@users.noreply.github.com>
2024-08-14 19:05:05 +0700

    aaaaaaaaaa

---
+++ src/main/java/jpa/Main.java
@@ -0,0 +1,100 @@
+package jpa;
+
+import jpa.configuration.SpringConfig;
+import jpa.entity.BookEntity;
+import jpa.entity.CategoryEntity;
+import jpa.repository.BookRepository;
+import jpa.repository.CategoryRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class Main {
+    static ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+    static BookRepository bookRepository = (BookRepository) context.getBean("bookRepository");
+    static CategoryRepository categoryRepository = (CategoryRepository) context.getBean(("categoryRepository"));
+    public static void main(String[] args){
+//        createNewBookEntityWithNewCategory();
+        createNewBookEntity();
+//        findByAuthor("Roger");
+//        findByNameAndAuthor("lunix", "Roger");
+//        findByNameOrAuthor("lunix", "Roger");
+//        findByPriceLessThan(80);
+//        findByBookDetailsIsbn("ISIBF1219321");
+//        findByNameContaining("Nu");
+//        findAllUsingQuery();
+    }
+//    public static void findByAuthor(String author){
+//        List<BookEntity> bookEntityList = bookRepository.findByAuthor(author);
+//        if (bookEntityList != null) {
+//            System.out.println("\nFind " + bookEntityList.size() + "books with author = " + author);
+//            for (BookEntity bookEntity: bookEntityList){
+//                System.out.println(bookEntity.toString());
+//            }
+//        }
+//    }
+//
+//    public static void findByNameAndAuthor (String name, String author){
+//        List<BookEntity> bookEntityList = bookRepository.findByNameAndAuthor(name, author);
+//        if (bookEntityList != null) {
+//            System.out.println("\nFind " + bookEntityList.size() + "books with name = " + name + "and author = " + author);
+//            for (BookEntity bookEntity: bookEntityList){
+//                System.out.println(bookEntity.toString());
+//            }
+//        }
+//    }
+//
+//    public static void findByNameOrAuthor(String name, String author) {
+//        List<BookEntity> bookEntityList = bookRepository.findByNameOrAuthor(name,author);
+//        if (bookEntityList != null) {
+//            System.out.println("\nFind " + bookEntityList.size() + "books with name = " + name + "and author = " + author);
+//            for (BookEntity bookEntity: bookEntityList){
+//                System.out.println(bookEntity.toString());
+//            }
+//        }
+//    }
+
+//    public static void findByNameContaining(String name) {
+//        List<BookEntity> bookEntityList = bookRepository.findByNameContaining(name);
+//        if (bookEntityList != null) {
+//            System.out.println("\nFind " + bookEntityList.size() + "books with containing name = " + name);
+//            for (BookEntity bookEntity: bookEntityList){
+//                System.out.println(bookEntity.toString());
+//            }
+//        }
+//    }
+
+//    public static void findAllUsingQuery() {
+//        List<BookEntity> bookEntityList = bookRepository.findAll();
+//        if (bookEntityList != null) {
+//            System.out.println("\nFind " + bookEntityList.size() + "books");
+//            for (BookEntity bookEntity: bookEntityList){
+//                System.out.println(bookEntity.toString());
+//            }
+//        }
+//    }
+
+//        public static void findByBookDetailsIsbn(String isbn) {
+//        BookEntity bookEntityList = bookRepository.findByBookDetailsIsbn(isbn);
+//        if (bookEntityList != null) {
+//            System.out.println("\nFind book with isbn" + isbn);
+//            System.out.println(bookEntityList.toString());
+//        }
+//    }
+
+    public static void createNewBookEntity() {
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setId(1);
+
+        BookEntity bookEntity = createNewBook();
+        bookEntity.setCategory(categoryEntity);
+        bookRepository.save(bookEntity);
+
+    }
+    public static BookEntity createNewBook() {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setAuthor("Sample Author");
+        return bookEntity;
+    }
+}
---
+++ src/main/java/jpa/entity/CategoryEntity.java
@@ -0,0 +1,53 @@
+package jpa.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "descriptione")
+    private String description;
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<BookEntity> bookEntityList;
+
+    public  CategoryEntity(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<BookEntity> getBookEntityList() {
+        return bookEntityList;
+    }
+
+    public void setBookEntityList(List<BookEntity> bookEntityList) {
+        this.bookEntityList = bookEntityList;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
